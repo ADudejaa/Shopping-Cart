965d6bba68dc88a0e549df6796f7b996b10fcd27
Archit Dudeja <39601517+De00@users.noreply.github.com>
2020-04-13 15:36:52 +0530

    Add files via upload

---
+++ cart/products.php
@@ -0,0 +1,248 @@
+<?php
+    session_start();
+    require 'check_if_added.php';
+?>
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Shopping Cart Example</title>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <!-- latest compiled and minified CSS -->
+        <link rel="stylesheet" href="bootstrap/css/bootstrap.min.css" type="text/css">
+        <!-- jquery library -->
+        <script type="text/javascript" src="bootstrap/js/jquery-3.2.1.min.js"></script>
+        <!-- Latest compiled and minified javascript -->
+        <script type="text/javascript" src="bootstrap/js/bootstrap.min.js"></script>
+        <!-- External CSS -->
+        <link rel="stylesheet" href="css/style.css" type="text/css">
+    </head>
+    <body>
+        <div>
+            <?php
+                require 'header.php';
+            ?>
+            <div class="container">
+                <div class="jumbotron">
+                    <img src="img/show6.png" style="postion:relative;margin-left:1%;height:30%;width:50%;"><img src="img/productslogo.jpeg" style="postion:relative;margin-right:1%;height:30%;width:35%;float:right ;">
+                </div>
+            </div>
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-3 col-sm-6">
+                        <div class="thumbnail">
+                                <img src="img/core ui.jpeg" alt="Core UI">
+                            <center>
+                                <div class="caption">
+                                    <h3>Core UI</h3>
+                                    <p>Price: Rs. 36000.00</p>
+                                    <?php if(!isset($_SESSION['email'])){  ?>
+                                        <p><a href="login.php" role="button" class="btn btn-primary btn-block">Buy Now</a></p>
+                                        <?php
+                                        }
+                                        else{
+                                            if(check_if_added_to_cart(1)){
+                                                echo '<a href="#" class=btn btn-block btn-success disabled>Added to cart</a>';
+                                            }else{
+                                                ?>
+                                                <a href="cart_add.php?id=1" class="btn btn-block btn-primary" name="add" value="add" class="btn btn-block btr-primary">Add to cart</a>
+                                                <?php
+                                            }
+                                        }
+                                        ?>
+                                </div>
+                            </center>
+                        </div>
+                    </div>
+                    <div class="col-md-3 col-sm-6">
+                        <div class="thumbnail">
+                                <img src="img/advancedui.jpg" alt="Advanced UI">
+                            <center>
+                                <div class="caption">
+                                    <h3>Advanced UI</h3>
+                                    <p>Price: Rs. 40000.00</p>
+                                    <?php if(!isset($_SESSION['email'])){  ?>
+                                        <p><a href="login.php" role="button" class="btn btn-primary btn-block">Buy Now</a></p>
+                                        <?php
+                                        }
+                                        else{
+                                            if(check_if_added_to_cart(2)){
+                                                echo '<a href="#" class=btn btn-block btn-success disabled>Added to cart</a>';
+                                            }else{
+                                                ?>
+                                                <a href="cart_add.php?id=2" class="btn btn-block btn-primary" name="add" value="add" class="btn btn-block btr-primary">Add to cart</a>
+                                                <?php
+                                            }
+                                        }
+                                        ?>
+                                </div>
+                            </center>
+                        </div>
+                    </div>
+                    <div class="col-md-3 col-sm-6">
+                        <div class="thumbnail">
+                                <img src="img/angular.png" alt="Angular JS">
+                            <center>
+                                <div class="caption">
+                                    <h3>Angular JS</h3>
+                                    <p>Price: Rs. 50000.00</p>
+                                    <?php if(!isset($_SESSION['email'])){  ?>
+                                        <p><a href="login.php" role="button" class="btn btn-primary btn-block">Buy Now</a></p>
+                                        <?php
+                                        }
+                                        else{
+                                            if(check_if_added_to_cart(3)){
+                                                echo '<a href="#" class=btn btn-block btn-success disabled>Added to cart</a>';
+                                            }else{
+                                                ?>
+                                                <a href="cart_add.php?id=3" class="btn btn-block btn-primary" name="add" value="add" class="btn btn-block btr-primary">Add to cart</a>
+                                                <?php
+                                            }
+                                        }
+                                        ?>
+                                </div>
+                            </center>
+                        </div>
+                    </div>
+                    <div class="col-md-3 col-sm-6">
+                        <div class="thumbnail">
+                                <img src="img/vue.jpg" alt="Vue">
+                            <center>
+                                <div class="caption">
+                                    <h3>Vue Js</h3>
+                                    <p>Price: Rs. 80000.00</p>
+                                    <?php if(!isset($_SESSION['email'])){  ?>
+                                        <p><a href="login.php" role="button" class="btn btn-primary btn-block">Buy Now</a></p>
+                                        <?php
+                                        }
+                                        else{
+                                            if(check_if_added_to_cart(4)){
+                                                echo '<a href="#" class=btn btn-block btn-success disabled>Added to cart</a>';
+                                            }else{
+                                                ?>
+                                                <a href="cart_add.php?id=4" class="btn btn-block btn-primary " name="add" value="add" class="btn btn-block btr-primary">Add to cart</a>
+                                                <?php
+                                            }
+                                        }
+                                        ?>
+                                </div>
+                            </center>
+                        </div>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-md-3 col-sm-6">
+                        <div class="thumbnail">
+                                <img src="img/react2.jpg" alt="React">
+                            <center>
+                                <div class="caption">
+                                    <h3>React JS</h3>
+                                    <p>Price: Rs. 13000.00</p>
+                                    <?php if(!isset($_SESSION['email'])){  ?>
+                                        <p><a href="login.php" role="button" class="btn btn-primary btn-block">Buy Now</a></p>
+                                        <?php
+                                        }
+                                        else{
+                                            if(check_if_added_to_cart(5)){
+                                                echo '<a href="#" class=btn btn-block btn-success disabled>Added to cart</a>';
+                                            }else{
+                                                ?>
+                                                <a href="cart_add.php?id=5" class="btn btn-block btn-primary " name="add" value="add" class="btn btn-block btr-primary">Add to cart</a>
+                                                <?php
+                                            }
+                                        }
+                                        ?>
+                                </div>
+                            </center>
+                        </div>
+                    </div>
+                    <div class="col-md-3 col-sm-6">
+                        <div class="thumbnail">
+                                <img src="img/java1.jpg" alt="Java">
+                            <center>
+                                <div class="caption">
+                                    <h3>Java</h3>
+                                    <p>Price: Rs. 3000.00</p>
+                                    <?php if(!isset($_SESSION['email'])){  ?>
+                                        <p><a href="login.php" role="button" class="btn btn-primary btn-block">Buy Now</a></p>
+                                        <?php
+                                        }
+                                        else{
+                                            if(check_if_added_to_cart(6)){
+                                                echo '<a href="#" class=btn btn-block btn-success disabled>Added to cart</a>';
+                                            }else{
+                                                ?>
+                                                <a href="cart_add.php?id=6" class="btn btn-block btn-primary " name="add" value="add" class="btn btn-block btr-primary">Add to cart</a>
+                                                <?php
+                                            }
+                                        }
+                                        ?>
+                                </div>
+                            </center>
+                        </div>
+                    </div>
+                    <div class="col-md-3 col-sm-6">
+                        <div class="thumbnail">
+                                <img src="img/net_1.jpg" alt="NET">
+                            <center>
+                                <div class="caption">
+                                    <h3>.NET</h3>
+                                    <p>Price: Rs. 8000.00</p>
+                                    <?php if(!isset($_SESSION['email'])){  ?>
+                                        <p><a href="login.php" role="button" class="btn btn-primary btn-block">Buy Now</a></p>
+                                        <?php
+                                        }
+                                        else{
+                                            if(check_if_added_to_cart(7)){
+                                                echo '<a href="#" class=btn btn-block btn-success disabled>Added to cart</a>';
+                                            }else{
+                                                ?>
+                                                <a href="cart_add.php?id=7" class="btn btn-block btn-primary " name="add" value="add" class="btn btn-block btr-primary">Add to cart</a>
+                                                <?php
+                                            }
+                                        }
+                                        ?>
+                                </div>
+                            </center>
+                        </div>
+                    </div>
+                    <div class="col-md-3 col-sm-6">
+                        <div class="thumbnail">
+                                <img src="img/nodejs.jpg" alt="Node">
+                            <center>
+                                <div class="caption">
+                                    <h3>NodeJS</h3>
+                                    <p>Price: Rs. 18000.00</p>
+                                    <?php if(!isset($_SESSION['email'])){  ?>
+                                        <p><a href="login.php" role="button" class="btn btn-primary btn-block">Buy Now</a></p>
+                                        <?php
+                                        }
+                                        else{
+                                            if(check_if_added_to_cart(8)){
+                                                echo '<a href="#" class=btn btn-block btn-success disabled>Added to cart</a>';
+                                            }else{
+                                                ?>
+                                                <a href="cart_add.php?id=8" class="btn btn-block btn-primary " name="add" value="add" class="btn btn-block btr-primary">Add to cart</a>
+                                                <?php
+                                            }
+                                        }
+                                        ?>
+                                </div>
+                            </center>
+                        </div>
+                    </div>
+                </div>
+                   
+                </div>
+            </div>
+            <br><br><br><br><br><br><br><br>
+           <footer class="footer">
+               <div class="container">
+                <center>
+                   <p>Developed by De Hundred </p>
+               </center>
+               </div>
+           </footer>
+        </div>
+    </body>
+</html>
